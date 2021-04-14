262e1620f5227896b718e6692c04f0285d628b5d
Snikers97 <zurabyan1@gmail.com>
2021-04-14 15:19:20 +0400

    add comment

--- parts/navigation.php
+++ parts/navigation.php
@@ -7,25 +7,39 @@
           <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
             <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
               <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
-              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;"><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
-</symbol>
-</defs></svg>
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <defs>
+                  <symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;">
+                    <rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect>
+                    <rect y="13" width="16" height="2"></rect>
+                  </symbol>
+                </defs>
+              </svg>
             </a>
           </div>
           <div class="u-custom-menu u-nav-container">
-            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Գլխավոր.html" style="padding: 10px 20px;">Գլխավոր</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Մեր-մասին.html" style="padding: 10px 20px;">Մեր մասին</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Հետադարձ-կապ.html" style="padding: 10px 20px;">Հետադարձ կապ</a>
-</li></ul>
+            <ul class="u-nav u-unstyled u-nav-1">
+              <li class="u-nav-item">
+                <a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Գլխավոր.html" style="padding: 10px 20px;">Գլխավոր</a>
+              </li>
+              <li class="u-nav-item">
+                <a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Մեր-մասին.html" style="padding: 10px 20px;">Մեր մասին</a>
+              </li>
+              <li class="u-nav-item">
+                <a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Հետադարձ-կապ.html" style="padding: 10px 20px;">Հետադարձ կապ</a>
+              </li>
+            </ul>
           </div>
+<!-- navigation bar for small screens -->
           <div class="u-custom-menu u-nav-container-collapse">
             <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
               <div class="u-sidenav-overflow">
                 <div class="u-menu-close"></div>
-                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Գլխավոր.html" style="padding: 10px 20px;">Գլխավոր</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Մեր-մասին.html" style="padding: 10px 20px;">Մեր մասին</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Հետադարձ-կապ.html" style="padding: 10px 20px;">Հետադարձ կապ</a>
-</li></ul>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2">
+                  <li class="u-nav-item"><a class="u-button-style u-nav-link" href="Գլխավոր.html" style="padding: 10px 20px;">Գլխավոր</a></li>
+                  <li class="u-nav-item"><a class="u-button-style u-nav-link" href="Մեր-մասին.html" style="padding: 10px 20px;">Մեր մասին</a></li>
+                  <li class="u-nav-item"><a class="u-button-style u-nav-link" href="Հետադարձ-կապ.html" style="padding: 10px 20px;">Հետադարձ կապ</a></li>
+                </ul>
               </div>
             </div>
             <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
