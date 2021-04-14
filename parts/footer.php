0a2fa34a34ae547eb723ed26bc468165e70cf81d
Snikers97 <zurabyan1@gmail.com>
2021-04-14 15:19:01 +0400

    add some lines in footer and chack styles

--- parts/footer.php
+++ parts/footer.php
@@ -1,8 +1,10 @@
 <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-1572"><div class="u-clearfix u-sheet u-sheet-1">
         <p class="u-small-text u-text u-text-variant u-text-1">Ստեղծագործ հոգեբանություն ՍՊԸ</p>
-        <p class="u-small-text u-text u-text-variant u-text-2">Բոլոր իրավունքները պաշտպանված են 2021</p>
-        <a class="u-link" href="https://nicepage.com/" target="_blank">
-        <span>Website Builder Software</span>
-      </a>
+        <p class="u-small-text u-text u-text-variant u-text-3">Բոլոր իրավունքները պաշտպանված են 2021</p>
+        <p class="u-small-text u-text u-text-variant u-text-2">
+          <a href="https://nicepage.com/" target="_blank">
+            <span>by Snikers97</span>
+          </a>
+        </p>
       </div>
     </footer>
