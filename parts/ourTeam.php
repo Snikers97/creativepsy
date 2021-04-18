850971b8aba78b47e7578c13e89f9d1908efa3d9
Snikers97 <zurabyan1@gmail.com>
2021-04-19 02:12:27 +0400

    we rewrite blcok of team

--- parts/ourTeam.php
+++ parts/ourTeam.php
@@ -1,9 +1,25 @@
 <section class="container-fluid customBgcolor">
-    <div class="container">
-        <div class="row">
+    <div class="container" style="display:flex; flex-wrpa:wrap;">
+        <div class="col">
             <div class="col-xxl text-center">
                 <h1 class="customTitle customToppaddingH1">Our Team</h1>
                 <p class="customDesc customToppaddingH2">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="col-xxl text-center p-5">
+                <div class="row">
+                    <div class="col-xl-4 p-0">
+                        <img src="/images/ourTeam/OTSOUE0g.jpg" class="w-100 imgHeight">
+                        <div class="w-100 bg-white imgHeight">info1</div>
+                    </div>
+                    <div class="col-xl-4 p-0">
+                        <div class="w-100 bg-white imgHeight">info2</div>
+                        <img src="/images/ourTeam/11.jpg" class="w-100 imgHeight">
+                    </div>
+                    <div class="col-xl-4 p-0">
+                        <img src="/images/ourTeam/de.jpg" class="w-100 imgHeight">
+                        <div class="w-100 bg-white imgHeight">info3</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
