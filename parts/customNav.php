84ec00a2b1e239c44142eda001ec917cfdf11cdb
Snikers97 <zurabyan1@gmail.com>
2021-04-18 20:32:03 +0400

    We rewrite nav style

---
+++ parts/customNav.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-text" style="height:74px;">
+    <div class="container" style="max-width: 1164px;">
+        <div class="navbar-header">
+            <a href="https://creativepsy.am" class="navbar-brand">
+                <img src="/images/default-logo.png" alt="" class="logoimg">
+            </a>
+        </div>
+        <div>
+            <ul class="nav justify-content-end">
+                <li class="nav-item  p-2">
+                    <a class="nav-link customMenucolorBlue active" aria-current="page" href="#">Գլխավոր</a>
+                </li>
+                <li class="nav-item  p-2">
+                    <a class="nav-link text-dark" aria-current="page" href="#">Մեր մասին</a>
+                </li>
+                <li class="nav-item  p-2">
+                    <a class="nav-link text-dark" aria-current="page" href="#">Հետադարձ կապ</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
