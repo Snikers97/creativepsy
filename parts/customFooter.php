bf6bbe9af357d67cc0dcb707b6b0d100a3ce4485
Snikers97 <zurabyan1@gmail.com>
2021-04-19 01:26:06 +0400

    We rewrite footer

---
+++ parts/customFooter.php
@@ -0,0 +1,11 @@
+<footer class="container-fluid footer customBgfooter">
+    <div class="container p-4 text-center">
+        <p class="text-white">Ստեղծագործ հոգեբանություն ՍՊԸ</p>
+        <p class="text-white">Բոլոր իրավունքները պաշտպանված են 2021</p>
+        <p class="text-white">
+          <a href="https://nicepage.com/" target="_blank">
+            <span>by Snikers97</span>
+          </a>
+        </p>
+    </div>
+</footer>
