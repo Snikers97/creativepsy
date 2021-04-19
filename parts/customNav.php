a377c8a5583f255d3841391582e02b6dd0591f02
Snikers97 <zurabyan1@gmail.com>
2021-04-20 01:02:21 +0400

    *

--- parts/customNav.php
+++ parts/customNav.php
@@ -1,20 +1,20 @@
 <nav class="navbar navbar-text" style="height:80px;">
     <div class="container" style="max-width: 1164px;">
         <div class="navbar-header">
-            <a href="https://creativepsy.am" class="navbar-brand">
+            <a href="https://creativepsy.am" class="navLogo">
                 <img src="/images/default-logo.png" alt="" class="logoimg">
             </a>
         </div>
         <div>
             <ul class="nav justify-content-end">
-                <li class="nav-item  p-2">
-                    <a class="nav-link customMenucolorBlue active" aria-current="page" href="#">Գլխավոր</a>
+                <li class="nav-item">
+                    <a class="nav-link customMenucolorBlue active" aria-current="page" href="/index.php">Գլխավոր</a>
                 </li>
-                <li class="nav-item  p-2">
-                    <a class="nav-link text-dark" aria-current="page" href="#">Մեր մասին</a>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" aria-current="page" href="aboutUs.php">Մեր մասին</a>
                 </li>
-                <li class="nav-item  p-2">
-                    <a class="nav-link text-dark" aria-current="page" href="#">Հետադարձ կապ</a>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" aria-current="page" href="/index.php#carousel_a7df">Հետադարձ կապ</a>
                 </li>
             </ul>
         </div>
