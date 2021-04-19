e6fb039d5c537c87f714856ff05be140b2b4adc9
Snikers97 <zurabyan1@gmail.com>
2021-04-19 17:34:52 +0400

    we fully rewrite team members block

--- parts/ourTeam.php
+++ parts/ourTeam.php
@@ -1,26 +1,124 @@
 <section class="container-fluid customBgcolor">
-    <div class="container" style="display:flex; flex-wrpa:wrap;">
-        <div class="col">
-            <div class="col-xxl text-center">
-                <h1 class="customTitle customToppaddingH1">Our Team</h1>
+    <div class="container" style="display: flex; flex-direction: column;">
+                <h1 class="customTitle customToppaddingH1">Մեր Թիմը</h1>
                 <p class="customDesc customToppaddingH2">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-            <div class="col-xxl text-center p-5">
-                <div class="row">
-                    <div class="col-xl-4 p-0">
-                        <img src="/images/ourTeam/OTSOUE0g.jpg" class="w-100 imgHeight">
-                        <div class="w-100 bg-white imgHeight">info1</div>
-                    </div>
-                    <div class="col-xl-4 p-0">
-                        <div class="w-100 bg-white imgHeight">info2</div>
-                        <img src="/images/ourTeam/11.jpg" class="w-100 imgHeight">
-                    </div>
-                    <div class="col-xl-4 p-0">
-                        <img src="/images/ourTeam/de.jpg" class="w-100 imgHeight">
-                        <div class="w-100 bg-white imgHeight">info3</div>
-                    </div>
+            <div class="col-xxl teamMembers">
+                <div class="row team">
+                            <div class="col-xl-4 p-0">
+                                <img src="/images/ourTeam/OTSOUE0g.jpg" class="w-100 imgHeight">
+                                <div class="w-100 bg-white imgHeight" id="member">
+                                    <h3>JEFFREY BROWN</h3>
+                                    <p class="position">creative leader</p>
+                                    <p class="aboutMember">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum</p>
+                                    <div class="socialIcon">
+                                        <a href="">
+                                            <img src="/images/socialIcons/facebook.png" alt="">
+                                        </a>
+                                        <a href="">
+                                            <img src="/images/socialIcons/instagram.png" alt="">
+                                        </a>
+                                        <a href="">
+                                            <img src="/images/socialIcons/telegram.png" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 p-0">
+                                <div class="w-100 bg-white imgHeight" id="member">
+                                    <h3>ALEX GREENFIELD</h3>
+                                    <p class="position">creative leader</p>
+                                    <p class="aboutMember">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum</p>
+                                    <div class="socialIcon">
+                                        <a href="">
+                                            <img src="/images/socialIcons/facebook.png" alt="">
+                                        </a>
+                                        <a href="">
+                                            <img src="/images/socialIcons/instagram.png" alt="">
+                                        </a>
+                                        <a href="">
+                                            <img src="/images/socialIcons/telegram.png" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                                <img src="/images/ourTeam/11.jpg" class="w-100 imgHeight">
+                            </div>
+                            <div class="col-xl-4 p-0">
+                                <img src="/images/ourTeam/de.jpg" class="w-100 imgHeight">
+                                <div class="w-100 bg-white imgHeight" id="member">
+                                    <h3>ANN RICHMOND</h3>
+                                    <p class="position">manager</p>
+                                    <p class="aboutMember">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum</p>
+                                    <div class="socialIcon">
+                                        <a href="">
+                                            <img src="/images/socialIcons/facebook.png" alt="">
+                                        </a>
+                                        <a href="">
+                                            <img src="/images/socialIcons/instagram.png" alt="">
+                                        </a>
+                                        <a href="">
+                                            <img src="/images/socialIcons/telegram.png" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 p-0">
+                                <img src="/images/ourTeam/OTSOUE0g.jpg" class="w-100 imgHeight">
+                                <div class="w-100 bg-white imgHeight" id="member">
+                                    <h3>JEFFREY BROWN</h3>
+                                    <p class="position">creative leader</p>
+                                    <p class="aboutMember">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum</p>
+                                    <div class="socialIcon">
+                                        <a href="">
+                                            <img src="/images/socialIcons/facebook.png" alt="">
+                                        </a>
+                                        <a href="">
+                                            <img src="/images/socialIcons/instagram.png" alt="">
+                                        </a>
+                                        <a href="">
+                                            <img src="/images/socialIcons/telegram.png" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 p-0">
+                                <div class="w-100 bg-white imgHeight" id="member">
+                                    <h3>ALEX GREENFIELD</h3>
+                                    <p class="position">creative leader</p>
+                                    <p class="aboutMember">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum</p>
+                                    <div class="socialIcon">
+                                        <a href="">
+                                            <img src="/images/socialIcons/facebook.png" alt="">
+                                        </a>
+                                        <a href="">
+                                            <img src="/images/socialIcons/instagram.png" alt="">
+                                        </a>
+                                        <a href="">
+                                            <img src="/images/socialIcons/telegram.png" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                                <img src="/images/ourTeam/11.jpg" class="w-100 imgHeight">
+                            </div>
+                            <div class="col-xl-4 p-0">
+                                <img src="/images/ourTeam/de.jpg" class="w-100 imgHeight">
+                                <div class="w-100 bg-white imgHeight" id="member">
+                                    <h3>ANN RICHMOND</h3>
+                                    <p class="position">manager</p>
+                                    <p class="aboutMember">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum</p>
+                                    <div class="socialIcon">
+                                        <a href="">
+                                            <img src="/images/socialIcons/facebook.png" alt="">
+                                        </a>
+                                        <a href="">
+                                            <img src="/images/socialIcons/instagram.png" alt="">
+                                        </a>
+                                        <a href="">
+                                            <img src="/images/socialIcons/telegram.png" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
                 </div>
             </div>
-        </div>
     </div>
 </section>
