334214457a97ab93ab2759a33ff2ec066db24525
Snikers97 <zurabyan1@gmail.com>
2021-04-21 00:23:55 +0400

    we finally finish team members block

--- parts/ourTeam.php
+++ parts/ourTeam.php
@@ -4,10 +4,10 @@
                 <p class="customDesc customToppaddingH2">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <div class="col-xxl teamMembers">
                 <div class="row">
-                    <div class="col-xl-4 col-lg-4 p-0">
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 p-0 card-block">
                     <div class="card border-0 rounded-0">
                         <img src="/images/ourTeam/OTSOUE0g.jpg" class="card-img-top rounded-0" alt="...">
-                        <div class="card-body text-center">
+                        <div class="card-body text-center cardInfoblock w-100 h-100">
                             <h5 class="card-title membersName">JEFFREY BROWN</h5>
                             <p class="card-text position">creative leader</p>
                             <p class="card-text aboutMember">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum</p>
@@ -25,9 +25,9 @@
                         </div>
                     </div>
                     </div>
-                    <div class="col-xl-4 col-lg-4 p-0">
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 p-0 card-block">
                     <div class="card border-0 rounded-0">
-                        <div class="card-body text-center">
+                        <div class="card-body text-center cardInfoblock w-100 h-100">
                             <h5 class="card-title membersName">JEFFREY BROWN</h5>
                             <p class="card-text position">creative leader</p>
                             <p class="card-text aboutMember">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum</p>
@@ -46,10 +46,10 @@
                         <img src="/images/ourTeam/OTSOUE0g.jpg" class="card-img-bottom rounded-0" alt="...">
                     </div>
                     </div>
-                    <div class="col-xl-4 col-lg-4 p-0">
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 p-0 card-block">
                     <div class="card border-0 rounded-0">
                         <img src="/images/ourTeam/OTSOUE0g.jpg" class="card-img-top rounded-0" alt="...">
-                        <div class="card-body text-center">
+                        <div class="card-body text-center cardInfoblock w-100 h-100">
                             <h5 class="card-title membersName">JEFFREY BROWN</h5>
                             <p class="card-text position">creative leader</p>
                             <p class="card-text aboutMember">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum</p>
@@ -67,10 +67,10 @@
                         </div>
                     </div>
                     </div>
-                    <div class="col-xl-4 col-lg-4 p-0">
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 p-0 card-block">
                     <div class="card border-0 rounded-0">
                         <img src="/images/ourTeam/OTSOUE0g.jpg" class="card-img-top rounded-0" alt="...">
-                        <div class="card-body text-center">
+                        <div class="card-body text-center cardInfoblock w-100 h-100">
                             <h5 class="card-title membersName">JEFFREY BROWN</h5>
                             <p class="card-text position">creative leader</p>
                             <p class="card-text aboutMember">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum</p>
@@ -88,9 +88,9 @@
                         </div>
                     </div>
                     </div>
-                    <div class="col-xl-4 col-lg-4 p-0">
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 p-0 card-block">
                     <div class="card border-0 rounded-0">
-                        <div class="card-body text-center">
+                        <div class="card-body text-center cardInfoblock w-100 h-100">
                             <h5 class="card-title membersName">JEFFREY BROWN</h5>
                             <p class="card-text position">creative leader</p>
                             <p class="card-text aboutMember">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum</p>
@@ -109,11 +109,11 @@
                         <img src="/images/ourTeam/OTSOUE0g.jpg" class="card-img-bottom rounded-0" alt="...">
                     </div>
                     </div>
-                    <div class="col-xl-4 col-lg-4 p-0">
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 p-0 card-block">
                     <div class="card border-0 rounded-0">
                         <img src="/images/ourTeam/OTSOUE0g.jpg" class="card-img-top rounded-0" alt="...">
-                        <div class="card-body text-center">
-                            <h5 class="card-title membersName">JEFFREY BROWN</h5>
+                        <div class="card-body text-center cardInfoblock w-100 h-100">
+                            <h5 class="card-title membersName ">JEFFREY BROWN</h5>
                             <p class="card-text position">creative leader</p>
                             <p class="card-text aboutMember">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum</p>
                             <div class="socialIcon">
@@ -196,7 +196,7 @@
                                 </div>
                             </div>
                            
-                </div> -->
+                </div>
             </div>
     </div>
 </section>
