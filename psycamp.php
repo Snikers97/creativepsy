856c38fc799ac6a8cabff26dc52ce8927b1ac24a
Snikers97 <zurabyan1@gmail.com>
2021-04-25 00:35:54 +0400

    we added page construction

---
+++ psycamp.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="online training programs, Մեր նախագծերը, 1, 2, 3, 4, 5, 6, Life is Good, why us, Results &amp; Partnerships, ՄԵր գործընկերները, contact, Հետադարձ կապ, don't be shy">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Psycamp</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/mystyle.css" media="screen">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    
+    
+    <script type="application/ld+json">{
+        "@context": "http://creativepsy.am",
+        "@type": "Organization",
+        "name": "Creative Psychology LLC",
+        "url": "psycamp.php",
+        "logo": "images/default-logo.png"
+      }
+    </script>
+    <meta property="og:title" content="Psycamp">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#478ac9">
+    <link rel="canonical" href="index.php">
+    <meta property="og:url" content="index.php">
+  </head>
+  <body>
+    <?php 
+      include("parts/customNav.php"); 
+      
+      include("parts/customFooter.php");
+    ?>
+    <script class="u-script" type="text/javascript" src="js/jquery.js" defer=""></script>
+  </body>
+</html>
