d265f43dbfa3bbfa8c1ad3f911236253a587c975
Snikers97 <zurabyan1@gmail.com>
2021-04-30 14:45:52 +0400

    We added announcement widget

--- widgets/announcement.php
+++ widgets/announcement.php
@@ -1,4 +1,33 @@
-
+<div class="announcement-widget m-0">
+    
+    <div class="annonBlock-1">
+        <div class="row m-0">
+            <div class="col-xl-3 announcement-item p-0" style="">
+                    <h6>Անձնային աճ. ճանաչիր և ակտիվացրու ներքին ռեսուրսներդ</h6>
+            </div>
+            <div class="col-xl-3 announcement-item p-0" style="background-image: url('/images/test/q.jpg'); background-size: cover; min-height:278px;">
+                <h6>Անձնային աճ. ճանաչիր և ակտիվացրու ներքին ռեսուրսներդ</h6>
+            </div>
+            <div class="col-xl-3 announcement-item p-0" style="background-image: url('/images/test/g.jpg'); background-size: cover; min-height:278px;">
+                <h6>Անձնային աճ. ճանաչիր և ակտիվացրու ներքին ռեսուրսներդ</h6>
+            </div>
+            <div class="col-xl-3 announcement-item p-0" style="background-image: url('/images/test/m.jpg'); background-size: cover; min-height:278px;">
+                <h6>Անձնային աճ. ճանաչիր և ակտիվացրու ներքին ռեսուրսներդ</h6>
+            </div>
+            <div class="col-xl-3 announcement-item p-0" style="background-image: url('http://images02.nicepage.io/c461c07a441a5d220e8feb1a/ee77c4c44a545f51bfa839a1/erw.jpg'); background-size: cover; min-height:278px; flex:none;">
+                <h6>Անձնային աճ. ճանաչիր և ակտիվացրու ներքին ռեսուրսներդ</h6>
+            </div>
+        </div>
+    </div>
+    <div class="annonBlock-2">
+        <div class="col-xl-3 announcement-item p-0" style="background-image: url('/images/test/l.jpg'); background-size: cover; min-height:278px; height: 66.66666666%;">
+            <h6>Անձնային աճ. ճանաչիր և ակտիվացրու ներքին ռեսուրսներդ</h6>
+        </div>
+        <div class="col-xl-3 announcement-item p-0" style="background-image: url('/images/test/travel.jpg'); background-size: cover; min-height:278px;">
+            <h6>Անձնային աճ. ճանաչիր և ակտիվացրու ներքին ռեսուրսներդ</h6>
+        </div>
+    </div>
+</div>
 
 
 
