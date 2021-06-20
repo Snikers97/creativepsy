872e58bb6c6a0101683ff5fab89db71a6d0dd866
Snikers97 <zurabyan1@gmail.com>
2021-06-20 23:01:03 +0400

    we connect announcement block

--- widgets/announcement.php
+++ widgets/announcement.php
@@ -1,30 +1,7 @@
 <div class="announcement-widget m-0">
-    
     <div class="annonBlock-1">
         <div class="row m-0">
-            <div class="col-xl-3 announcement-item p-0" style="">
-                    <h6>Անձնային աճ. ճանաչիր և ակտիվացրու ներքին ռեսուրսներդ</h6>
-            </div>
-            <div class="col-xl-3 announcement-item p-0" style="background-image: url('/images/test/q.jpg'); background-size: cover; min-height:278px;">
-                <h6>Անձնային աճ. ճանաչիր և ակտիվացրու ներքին ռեսուրսներդ</h6>
-            </div>
-            <div class="col-xl-3 announcement-item p-0" style="background-image: url('/images/test/g.jpg'); background-size: cover; min-height:278px;">
-                <h6>Անձնային աճ. ճանաչիր և ակտիվացրու ներքին ռեսուրսներդ</h6>
-            </div>
-            <div class="col-xl-3 announcement-item p-0" style="background-image: url('/images/test/m.jpg'); background-size: cover; min-height:278px;">
-                <h6>Անձնային աճ. ճանաչիր և ակտիվացրու ներքին ռեսուրսներդ</h6>
-            </div>
-            <div class="col-xl-3 announcement-item p-0" style="background-image: url('/images/test/1DrIeh.jpg'); background-size: cover; min-height:278px; flex:none;">
-                <h6>Անձնային աճ. ճանաչիր և ակտիվացրու ներքին ռեսուրսներդ</h6>
-            </div>
-        </div>
-    </div>
-    <div class="annonBlock-2">
-        <div class="col-xl-3 announcement-item p-0" style="background-image: url('/images/test/l.jpg'); background-size: cover; min-height:278px; height: 66.66666666%;">
-            <h6>Անձնային աճ. ճանաչիր և ակտիվացրու ներքին ռեսուրսներդ</h6>
-        </div>
-        <div class="col-xl-3 announcement-item p-0" style="background-image: url('/images/test/travel.jpg'); background-size: cover; min-height:278px;">
-            <h6>Անձնային աճ. ճանաչիր և ակտիվացրու ներքին ռեսուրսներդ</h6>
+        <? getpsycampAnnouncement($db_connect); ?>
         </div>
     </div>
 </div>
